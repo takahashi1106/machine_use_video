cd1953e0ebe9636ebad8cf487d030eb43e5690ef
takahashi1106 <tk2696274@gmail.com>
2022-05-19 07:35:36 +0000

    [update]

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -26,7 +26,7 @@ class Public::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
   def after_sign_in_path_for(resource)
-    root_path
+    machines_path
   end
 
   def after_sign_out_path_for(resource)
