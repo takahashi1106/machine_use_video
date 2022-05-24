0e8de4c902ad23d526e683de0bf9174abc54ede5
takahashi1106 <tk2696274@gmail.com>
2022-05-24 06:07:11 +0000

    [update]

--- app/controllers/public/machines_controller.rb
+++ app/controllers/public/machines_controller.rb
@@ -9,18 +9,11 @@ class Public::MachinesController < ApplicationController
     @search = Machine.ransack(params[:q])
     #distinct: trueは重複したデータを除外
     @machiness = @search.result(distinct: true).limit(5)
-    # タグ検索
-    #@tag_search = Machine.tagged_with(params[:search])
   end
 
   def show #machine_path
     @machine = Machine.find(params[:id])
     @machine_comment = MachineComment.new
   end
-
-  #def search #search_machines_path
-    # タグ検索
-    #@tag_search = Machine.tagged_with(params[:search])
-  #end
 
 end
