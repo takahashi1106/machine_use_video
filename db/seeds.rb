864895359246c9a9913716ebb1a63cf12888d3a3
takahashi1106 <tk2696274@gmail.com>
2022-05-27 03:57:23 +0000

    [update]

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(
-    last_name: '管理者',
-    first_name: '太郎',
-    tel_number: '090-0000-0000',
+Admin.create!(
     email: 'test@test.com',
     password: '000000',
-    admin: true,
 )
