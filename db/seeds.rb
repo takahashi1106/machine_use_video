8c289a27ac85c04a30131134faf94898c5fd68f5
takahashi1106 <tk2696274@gmail.com>
2022-05-28 08:28:44 +0000

    [update]

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create!(
-    email: 'test@test.com',
-    password: '000000',
-)
+Admin.find_or_create_by!(
+    email: ENV['ADMIN_MAIL'],
+) do |admin|
+    admin.password = ENV['ADMIN_PASSWORD']
+end
+
