50f225a8ada23e05f9480298433e520c2c019a01
takahashi1106 <tk2696274@gmail.com>
2022-06-06 07:11:21 +0000

    [update}

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,5 @@
 class Public::HomesController < ApplicationController
-  before_action :authenticate_user!, except: [:top]
+  before_action :authenticate_user!, except: [:top, :about]
 
   def top
   end
