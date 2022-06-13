ddd627a8a95057d8ff2ade9495a173024ade2840
takahashi1106 <tk2696274@gmail.com>
2022-06-13 10:23:21 +0000

    [update]

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "../stylesheets/application"
+//import 'jquery'
+import "skippr.min"
+import "script.js"
 Rails.start()
 // Turbolinks.start()
 ActiveStorage.start()
